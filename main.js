012f228c360596d14030cc76e79b5d0e1cb1a3f0
Glitch (hello-jquery6) <none>
2020-11-18 14:11:32 +0000

    🍂🍰 Checkpoint ./main.js:1151468/26

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-$.getJSON('https://link to your JSON data set', function(json) {
-  console.log(json);
+$.getJSON('https://link to your JSON data set', function(myDataset) {
+  console.log(myDataset);
 });

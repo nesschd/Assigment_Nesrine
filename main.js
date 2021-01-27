517cf6817e25bf5d92b3753f691d3f1a49ffda0c
Glitch (hello-jquery6) <none>
2021-01-27 13:40:00 +0000

    🏆⚡️ Checkpoint ./index.html:26956789/1759 ./tenor.json:26956789/86987 ./main.js:26956789/869 ./style.css:26956789/340

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-$.getJSON("tenor.json", function(myDataset) {
+$.getJSON("https://api.tenor.com/v1/search?cats", function(myDataset) {
     var tenor = myDataset;
-    console.log(tenor.results.title);
-  $( "body" ).append( "<h1>" + tolkien.name + "</h1>" );
+    var gif = null;
+    for(i = 0; i < tenor.results.length; i++) {
+        title = JSON.stringify(tenor.results[i].title);
+        $("<input id=" + title + "type=button value=" + title + " > </input>").appendTo("#allTitles"); //appendTo: Append at inside bottom
+    }
+    $("input").click(function(e){
+        var idClicked = e.target.id;
+        for(i = 0; i < tenor.results.length; i++) {
+            if (tenor.results[i].title === idClicked){
+                gif = tenor.results[i].media[0].gif.url;
+            }
+        }
+        document.getElementById('myAnimatedImage').src = gif; 
+ });
 });
